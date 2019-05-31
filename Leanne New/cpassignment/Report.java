0252b74e04b9c40a665ead8793335bf49bfe81fb
Tan Lay Yan <36462525+leannetan97@users.noreply.github.com>
2019-05-31 15:55:00 +0800

    Update Report Class

--- Leanne New/cpassignment/Report.java
+++ Leanne New/cpassignment/Report.java
@@ -1,79 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cpassignment;
 
-/**
- *
- * @author Leanne
- */
-import java.util.ArrayList;
+public class Time {
 
-public class Report {
+    private long arrivalTime;
+    private long waitingTime;
+    private long consultationTime;
+    private long endOfWaitingTime;
 
-    private final HospitalManagement hospital;
-
-    public Report(HospitalManagement hospital) {
-        this.hospital = hospital;
+    public Time(long arrivalTime,long consultationTime) {
+        //Not system time
+        this.arrivalTime = arrivalTime;
+        this.consultationTime = consultationTime;
     }
 
-    public void generateReport() {
-        System.out.println("-----------------Report-----------------");
-        printNumberOfPatientVisitHospital();
-        printDoctorSummaryReport();
-        printOverallAverageConsultationTime();
-        printOverallAverageWaitingTimeOfPatient();
-        System.out.println("----------------------------------------");
+    // Getter
+    public long getWaitingTime() {
+        calculatedWaitingTime();
+        return waitingTime;
     }
 
-    // sum of total patient number for all doctor
-    public void printNumberOfPatientVisitHospital() {
-        System.out.println("Number of Patients: " + hospital.getNumberOfPatientVisit());
+    public long getConsultationTime() {
+        return consultationTime;
     }
 
-    // Number of patient seen by each doctor
-    // Average consultationTime by each doctor = totalConsultationTime of the doctor
-    // Average waitingTime of patient by each doctor = totalPatientWaitingTime of the doctor / totalPatientOf the doctor
-    public void printDoctorSummaryReport() {
-        ArrayList<Doctor> doctorList = hospital.getWorkingDoctorList();
-        for (int i = 1; i <= doctorList.size(); i++) {
-            System.out.println("Doctor " + i + " : ");
-            int total = doctorList.get(i).getTotalNumberOfPatient();
-            long consultation = doctorList.get(i).getTotalConsultationTime();
-            long wait = doctorList.get(i).getMyPatientWaitingTime();
-            System.out.println("Number of Patients: " + total);
-            System.out.println("Average Consultation Time: " + (consultation / total));
-            System.out.println("Average Waiting Time: " + (wait / total));
-            System.out.println("");
-        }
-
+    public long getArrivalTime() {
+        return arrivalTime;
     }
 
-    // sum of all patient Consultation Time/sum of total patient number for all doctor
-    public void printOverallAverageConsultationTime() {
-        ArrayList<Patient> patientList = new ArrayList<>();
-        //LY comment: why the patient list seems like empty?
-        
-        long totalConsultationTime = 0;
-        for (int i = 0; i < patientList.size(); i++) {
-            totalConsultationTime += patientList.get(i).getConsultationTime();
-        }
-        long overallAverageConsultationTime = totalConsultationTime / patientList.size();
-        System.out.println("Overall Average Consultation Time: " + overallAverageConsultationTime);
+    // Setter
+//    public void setArrivalTime(long arrivalTime) {
+//        this.arrivalTime = arrivalTime;
+//    }
+//
+//    public void setConsultationTime(long consultationTime) {
+//        this.consultationTime = consultationTime;
+//    }
+
+    public void setEndOfWaitingTime(long endOfWaitingTime) {
+        this.endOfWaitingTime = endOfWaitingTime;
     }
 
-    // sum of all patient Waiting Time /sum of total patient number for all doctor
-    public void printOverallAverageWaitingTimeOfPatient() {
-        ArrayList<Patient> patientList = new ArrayList<>();
-        //LY comment: why the patient list seems like empty?
-        long totalWaitingTime = 0;
-        for (int i = 0; i < patientList.size(); i++) {
-            totalWaitingTime += patientList.get(i).getWaitingTime();
-        }
-        long overallAverageWaitingTime = totalWaitingTime / patientList.size();
-        System.out.println("Overall Average Waiting Time: " + overallAverageWaitingTime);
+    // Calculation
+    private long calculatedWaitingTime() {
+        this.waitingTime = endOfWaitingTime - arrivalTime;
+        return waitingTime;
     }
 
 }
