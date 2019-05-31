ff25097177b6fcbf627f30bf409f9f71d803699b
Tan Lay Yan <36462525+leannetan97@users.noreply.github.com>
2019-05-31 15:57:27 +0800

    Update Time.java

--- Leanne New/cpassignment/Time.java
+++ Leanne New/cpassignment/Time.java
@@ -7,13 +7,15 @@ public class Time {
     private long consultationTime;
     private long endOfWaitingTime;
 
-    public Time() {
-        this.arrivalTime = System.currentTimeMillis();
+    public Time(long arrivalTime,long consultationTime) {
+        //Not system time
+        this.arrivalTime = arrivalTime;
+        this.consultationTime = consultationTime;
     }
 
     // Getter
     public long getWaitingTime() {
-        calcuatedWaitingTime();
+        calculatedWaitingTime();
         return waitingTime;
     }
 
@@ -26,20 +28,20 @@ public class Time {
     }
 
     // Setter
-    public void setArrivalTime(long arrivalTime) {
-        this.arrivalTime = arrivalTime;
-    }
-
-    public void setConsultationTime(long consultationTime) {
-        this.consultationTime = consultationTime;
-    }
+//    public void setArrivalTime(long arrivalTime) {
+//        this.arrivalTime = arrivalTime;
+//    }
+//
+//    public void setConsultationTime(long consultationTime) {
+//        this.consultationTime = consultationTime;
+//    }
 
     public void setEndOfWaitingTime(long endOfWaitingTime) {
         this.endOfWaitingTime = endOfWaitingTime;
     }
 
     // Calculation
-    private long calcuatedWaitingTime() {
+    private long calculatedWaitingTime() {
         this.waitingTime = endOfWaitingTime - arrivalTime;
         return waitingTime;
     }
